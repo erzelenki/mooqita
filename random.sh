87dabb04f4a65050f062cf8d38f1019ac9a5828e
Alex 2 <er@zlenki.net>
2020-04-13 23:11:31 +0000

    I made file shorter and nicer

--- random.sh
+++ random.sh
@@ -2,27 +2,35 @@
 #mooqita first challenge
 
 #Random size from 1 to 15 characters with A..Z - a..z - 0..9 String creation
-echo -n >  rndfile
-str_count=0
+
 while [[ $(( $str_count/1024 )) -lt 1000 ]]
 do
 
-str_size=$(( $RANDOM/2185+1 ))
-str_count=$(( $str_count+$str_size+1 ))
-cat /dev/urandom | tr -dc [:alnum:] | head -c $str_size >> rndfile
-echo >> rndfile
+	str_size=$(( $RANDOM/2185+1 ))
+	str_count=$(( $str_count+$str_size+1 ))
+	echo `cat /dev/urandom | tr -dc [:alnum:] | head -c $str_size` >> rndfile
+
 done
+
 echo "new file with random strings is rndfile"
+
+
 #Sorting  file
-cat | rndfile | sort -d -o rndsort
+cat rndfile | sort -d -o rndsort
 echo "sorted file with random strings is rndsort"
+
+
 #removing "a" from sorted file
 #cat testfile | xargs -I {} bash -c 'if [[ {} =~ [*a*] ]] ; then let counter++ ; else  echo {} >> newfile ; fi'
 #echo $counter
 #by some reason 'let counter++' is not increasing
+
+
+
+#removing "a" from sorted file more simple solution
 grep -v 'a' rndsort > rnd_without_a
 echo -n 'There has been deleted '
-grep 'a' rndsort | wc -l
+echo -n `grep 'a' rndsort | wc -l`
 echo " lines"
 echo "new sorted file without lines, containing 'a', is rnd_without_a" 
 exit 0
