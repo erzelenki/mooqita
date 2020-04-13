dd227c100d38e3fb61a346c951a1b09cf99391c3
Alex 2 <er@zlenki.net>
2020-04-13 18:52:57 +0000

    1Mb version without sorting

--- random.sh
+++ random.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 #mooqita first challenge
+
+#Random size from 1 to 15 characters with A..Z - a..z - 0..9 String creation
+echo -n >  rndfile
+str_count=0
+while [[ $(( $str_count/1024 )) -lt 1000 ]]
+do
+
+str_size=$(( $RANDOM/2185+1 ))
+str_count=$(( $str_count+$str_size+1 ))
+cat /dev/urandom | tr -dc [:alnum:] | head -c $str_size >> rndfile
+echo >> rndfile
+done
+ls -lh rndfile
+ls -l rndfile
+wc -c rndfile
